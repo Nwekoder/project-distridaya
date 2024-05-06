4057639661a683da82c2b7a89296eec16fd1c3ff
Nwekoder <cn1250425@gmail.com>
2024-05-06 15:36:25 +0800

    Mau bikin ulang deh

--- app_v1/src/pages/SupplierPage.jsx
+++ app_v1/src/pages/SupplierPage.jsx
@@ -1,8 +1,10 @@
 import {atom, useAtom} from 'jotai'
-import {ArrowDownRight, PlusIcon, Settings2Icon} from 'lucide-react'
+import {ArrowDownRight, EditIcon, LoaderCircle, PlusIcon, TrashIcon} from 'lucide-react'
 import { useEffect, useState } from 'react'
 
 const SupplierAddWindowAtom = atom(false)
+const SupplierDeleteDialogAtom = atom(false)
+const SupplierEditWindowAtom = atom(false)
 
 export default function SupplierPage() {
     const [data, setData] = useState([])
@@ -15,6 +17,8 @@ export default function SupplierPage() {
             }
         }).then(res => {
             if(res.ok) return res.json()
+        }).then(json => {
+            setData(json.data)
         })
     }, [data])
 
@@ -37,14 +41,15 @@ export default function SupplierPage() {
                         {data.map(d => (
                             <tr key={d._id} className='border-b border-gray-700 border-opacity-25 last:border-0'>
                                 <td className="px-4 py-2 text-sm whitespace-nowrap">{d.name}</td>
-                                <td className="px-4 py-2 text-sm whitespace-nowrap">{d.npwp}</td>
-                                <td className="px-4 py-2 text-sm whitespace-nowrap">{d.address}</td>
+                                <td className="px-4 py-2 text-sm text-center whitespace-nowrap">{d.npwp || '-'}</td>
+                                <td className="px-4 py-2 text-sm">{d.address}</td>
                                 <td className="px-4 py-2 text-sm whitespace-nowrap">{d.phone}</td>
-                                <td className="px-4 py-2 text-sm whitespace-nowrap">{d.email || ''}</td>
-                                <td className="flex items-center justify-center px-4 py-2 text-sm whitespace-nowrap">
-                                    <button type="button" className="flex items-center justify-center p-0 transition-[background] hover:bg-gray-900 size-8 btn">
-                                        <Settings2Icon className='size-4' />
+                                <td className="px-4 py-2 text-sm text-center whitespace-nowrap">{d.email || '-'}</td>
+                                <td className="flex items-center justify-center gap-2 px-4 py-2 text-sm whitespace-nowrap">
+                                    <button type="button" className="flex items-center peer justify-center p-0 transition-[background] hover:bg-gray-900 size-8 btn">
+                                        <EditIcon className='size-4' />
                                     </button>
+                                    <SupplierDeleteDialog supplier_id={d._id} />
                                 </td>
                             </tr>
                         ))}              
@@ -59,7 +64,42 @@ export default function SupplierPage() {
 
 function SupplierAddWindow() {
     const [open, setOpen] = useAtom(SupplierAddWindowAtom)
+    const [loading, setLoading] = useState(false)
     
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setLoading(true)
+    
+        const formData = new FormData(e.target)
+        const data = JSON.parse(JSON.stringify(Object.fromEntries(formData)))
+
+        const res = await fetch('http://localhost:8086/entity/supplier', {
+            method: "POST",
+            headers: {
+                "X-TDP-Authtoken": localStorage.getItem("authtoken"),
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                name: data.name,
+                npwp: data.npwp,
+                city: data.city,
+                address: data.address,
+                phone: data.phone,
+                email: data.email,
+                fax: data.fax,
+            })
+        })
+
+        if(res.ok) {
+            e.target.reset()
+            setLoading(false)
+        }else {
+            setLoading(false)
+            const json = await res.json()
+            alert(`Terjadi kesalahan! ${json.message}`)
+        }
+    }
+
     return (
         <>
             {open ? (
@@ -68,11 +108,90 @@ function SupplierAddWindow() {
                         <ArrowDownRight className='size-4' />
                     </button>
                     
-                    SupplierAddWindow
+                    {loading ? (
+                        <div className="flex items-center justify-center h-full px-2 py-4 overflow-hidden">
+                            <LoaderCircle className='size-8 animate-spin' />
+                        </div>
+                    ) : (
+                        <div className="flex flex-col h-full px-2 py-4 overflow-y-auto">
+                            <form onSubmit={handleSubmit}>
+                                <div className="text-input">
+                                    <label htmlFor="name">Nama Supplier</label>
+                                    <input type="text" name="name" id="name" required />
+                                </div>
+                                <div className="mb-8 text-input">
+                                    <label htmlFor="npwp">NPWP</label>
+                                    <input type="text" name="npwp" id="npwp" />
+                                </div>
+
+                                <div className="text-input">
+                                    <label htmlFor="city">Kota</label>
+                                    <input type="text" name="city" id="city" required />
+                                </div>
+                                <div className="mb-8 text-input">
+                                    <label htmlFor="address">Alamat</label>
+                                    <input type="text" name="address" id="address" required />
+                                </div>
+
+                                <div className="text-input">
+                                    <label htmlFor="phone">Phone</label>
+                                    <input type="tel" name="phone" id="phone" required />
+                                </div>
+                                <div className="text-input">
+                                    <label htmlFor="email">E-Mail</label>
+                                    <input type="email" name="email" id="email" />
+                                </div>
+                                <div className="mb-10 text-input">
+                                    <label htmlFor="email">Fax</label>
+                                    <input type="tel" name="fax" id="fax" />
+                                </div>
+
+                                <button type="submit" className="bg-blue-800 btn">Save</button>
+                            </form>
+                        </div>
+                    )}
                 </div>
             ) : (
                 <button type="button" onClick={() => setOpen(true)} className="fixed flex items-center justify-center text-xl font-bold bg-blue-800 transition-[background] rounded-full bottom-4 right-4 size-12">
                     <PlusIcon />
+                </button>
+            )}
+        </>
+    )
+}
+
+function SupplierDeleteDialog({supplier_id}) {
+    const [open, setOpen] = useAtom(SupplierDeleteDialogAtom)
+
+    async function handleDelete() {
+        const res = await fetch(`http://localhost:8086/entity/supplier/${supplier_id}`, {
+            method: "DELETE",
+            headers: {
+                "X-TDP-Authtoken": localStorage.getItem("authtoken"),
+            }
+        })
+
+        if(res.ok) {
+            setOpen(false)
+        }
+    }
+
+    return (
+        <>
+            {open ? (
+                <div className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-neutral-900 bg-opacity-30 backdrop-blur-md">
+                    <div className="p-4 bg-gray-800 rounded-md w-96">
+                        <p className='mb-8'>Apakah anda yakin ingin menghapus data ini?</p>
+
+                        <div className="flex items-center justify-evenly">
+                            <button type="button" onClick={handleDelete} className="bg-red-800 btn">Ya</button>
+                            <button type="button" onClick={() => setOpen(false)} className="btn">Tidak</button>
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <button onClick={() => setOpen(true)} type="button" className="flex items-center peer justify-center p-0 transition-[background] hover:bg-red-900 size-8 btn">
+                    <TrashIcon className='size-4' />
                 </button>
             )}
         </>
--- server/routers/entity/supplierRouter.js
+++ server/routers/entity/supplierRouter.js
@@ -1,18 +1,59 @@
 const {Router} = require('express')
-// const {client} = require('../../db')
-// const {z} = require('zod')
+const {client} = require('../../db')
+const {z} = require('zod')
+const { ObjectId } = require('mongodb')
 
 const supplierRouter = Router()
-// const db = client.db().collection('suppliers')
+const db = client.db().collection('suppliers')
 
 // Get All Supplier
 supplierRouter.get('/', async(req,res) => {
-    res.status(200).json({message: "TBA"})
+    await client.connect()
+    const suppliers = await db.find({}).toArray()
+    await client.close()
+    
+    res.status(200).json({message: "Success!", data: suppliers})
 })
 
 // Add new Supplier
 supplierRouter.post('/', async(req,res) => {
-    res.status(200).json({message: "TBA", id_supplier: req.params.id_supplier})
+    try {
+        const data = req.body
+        const schema = z.object({
+            name: z.string(),
+            npwp: z.string().optional(),
+            city: z.string(),
+            address: z.string(),
+            phone: z.string(),
+            email: z.string().optional(),
+            fax: z.string().optional(),
+        })
+
+        const validate = schema.parse(data)
+
+        await client.connect()
+
+        await db.insertOne({
+            name: validate.name,
+            npwp: validate.npwp,
+            city: validate.city,
+            address: validate.address,
+            phone: validate.phone,
+            email: validate.email,
+            fax: validate.fax,
+        })
+
+        await client.close()
+        
+        res.status(200).json({message: "Success!"})
+    } catch (error) {
+        if(error.issues) {
+            res.status(400).json({message: "Validation error", errors: error.issues.map(issue => issue.message)})
+        }else {
+            res.status(400).json({message: error.message})
+        }
+    }
+
 })
 
 // Get a Supplier by ID
@@ -27,7 +68,22 @@ supplierRouter.patch('/:id_supplier', async(req,res) => {
 
 // Delete Supplier by ID
 supplierRouter.delete('/:id_supplier', async(req,res) => {
-    res.status(200).json({message: "TBA", id_supplier: req.params.id_supplier})
+    try {
+        const supplier_id = req.params.id_supplier
+
+        await client.connect()
+        await db.deleteOne({_id: new ObjectId(supplier_id)})
+        await client.close()
+
+        res.status(200).json({message: "Success!"})
+    } catch (error) {
+        if(error.issues) {
+            res.status(400).json({message: "Validation error", errors: error.issues.map(issue => issue.message)})
+        }else {
+            res.status(400).json({message: error.message})
+        }
+        console.error(error)
+    }
 })
 
 module.exports = supplierRouter
